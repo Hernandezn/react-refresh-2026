805c3c067a27e1f8a6dda9a69889da3c44c6e3ec
hernandezn <hernandezn@live.com>
2026-04-16 18:42:26 -0400

    Completed touchups for database module & .env environment variables

--- nextjs-foundations-2026/nextjs-dashboard/app/query/route.ts
+++ nextjs-foundations-2026/nextjs-dashboard/app/query/route.ts
@@ -3,11 +3,19 @@ import postgres from 'postgres';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function listInvoices() {
+  // EQUIVALENT but DEPRECATED due to implicit join:
+  // const data = await sql`
+  //   SELECT i.amount, c.name
+  //   FROM invoices i, customers c
+  //   WHERE i.customer_id = c.id
+  //   AND invoices.amount = 3040;
+  // `;
+
 	const data = await sql`
     SELECT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
+    WHERE invoices.amount = 3040;
   `;
 
 	return data;
